db9485f84ef20d5baf7f4de639b87ea7706c530d
hasee <hasee@DESKTOP-MLSL96I>
2018-09-20 18:38:41 +0800

    添加震动全排列

--- wear/src/main/java/com/example/hasee/vibratorlock/vibratorlock/activity/VibActivity.java
+++ wear/src/main/java/com/example/hasee/vibratorlock/vibratorlock/activity/VibActivity.java
@@ -145,18 +145,167 @@ public class VibActivity extends Activity{
         list.add(vibArrUtil0);
 
         VibArrUtil vibArrUtil1 = new VibArrUtil();
-        vibArrUtil1.setIv_1st(id_longshort);
+        vibArrUtil1.setIv_1st(id_longlong);
         vibArrUtil1.setIv_2nd(id_longshort);
-        vibArrUtil1.setIv_3rd(id_shortlong);
-        vibArrUtil1.setIv_4th(id_shortshort);
+        vibArrUtil1.setIv_3rd(id_shortshort);
+        vibArrUtil1.setIv_4th(id_shortlong);
         list.add(vibArrUtil1);
 
         VibArrUtil vibArrUtil2 = new VibArrUtil();
-        vibArrUtil2.setIv_1st(id_longshort);
-        vibArrUtil2.setIv_2nd(id_longshort);
-        vibArrUtil2.setIv_3rd(id_shortlong);
-        vibArrUtil2.setIv_4th(id_longlong);
+        vibArrUtil2.setIv_1st(id_longlong);
+        vibArrUtil2.setIv_2nd(id_shortlong);
+        vibArrUtil2.setIv_3rd(id_longshort);
+        vibArrUtil2.setIv_4th(id_shortshort);
         list.add(vibArrUtil2);
+
+        VibArrUtil vibArrUtil3 = new VibArrUtil();
+        vibArrUtil3.setIv_1st(id_longlong);
+        vibArrUtil3.setIv_2nd(id_shortlong);
+        vibArrUtil3.setIv_3rd(id_shortshort);
+        vibArrUtil3.setIv_4th(id_longshort);
+        list.add(vibArrUtil3);
+
+        VibArrUtil vibArrUtil4 = new VibArrUtil();
+        vibArrUtil4.setIv_1st(id_longlong);
+        vibArrUtil4.setIv_2nd(id_shortshort);
+        vibArrUtil4.setIv_3rd(id_longshort);
+        vibArrUtil4.setIv_4th(id_shortlong);
+        list.add(vibArrUtil4);
+
+        VibArrUtil vibArrUtil5 = new VibArrUtil();
+        vibArrUtil5.setIv_1st(id_longlong);
+        vibArrUtil5.setIv_2nd(id_shortshort);
+        vibArrUtil5.setIv_3rd(id_shortlong);
+        vibArrUtil5.setIv_4th(id_longshort);
+        list.add(vibArrUtil5);
+
+        VibArrUtil vibArrUtil6 = new VibArrUtil();
+        vibArrUtil6.setIv_1st(id_longshort);
+        vibArrUtil6.setIv_2nd(id_longlong);
+        vibArrUtil6.setIv_3rd(id_shortlong);
+        vibArrUtil6.setIv_4th(id_shortshort);
+        list.add(vibArrUtil6);
+
+        VibArrUtil vibArrUtil7 = new VibArrUtil();
+        vibArrUtil7.setIv_1st(id_longshort);
+        vibArrUtil7.setIv_2nd(id_longlong);
+        vibArrUtil7.setIv_3rd(id_shortshort);
+        vibArrUtil7.setIv_4th(id_shortlong);
+        list.add(vibArrUtil7);
+
+        VibArrUtil vibArrUtil8 = new VibArrUtil();
+        vibArrUtil8.setIv_1st(id_longshort);
+        vibArrUtil8.setIv_2nd(id_shortlong);
+        vibArrUtil8.setIv_3rd(id_longlong);
+        vibArrUtil8.setIv_4th(id_shortshort);
+        list.add(vibArrUtil8);
+
+        VibArrUtil vibArrUtil9 = new VibArrUtil();
+        vibArrUtil9.setIv_1st(id_longshort);
+        vibArrUtil9.setIv_2nd(id_shortlong);
+        vibArrUtil9.setIv_3rd(id_shortshort);
+        vibArrUtil9.setIv_4th(id_longlong);
+        list.add(vibArrUtil9);
+
+        VibArrUtil vibArrUtil10 = new VibArrUtil();
+        vibArrUtil10.setIv_1st(id_longshort);
+        vibArrUtil10.setIv_2nd(id_shortshort);
+        vibArrUtil10.setIv_3rd(id_longlong);
+        vibArrUtil10.setIv_4th(id_shortlong);
+        list.add(vibArrUtil10);
+
+        VibArrUtil vibArrUtil11 = new VibArrUtil();
+        vibArrUtil11.setIv_1st(id_longshort);
+        vibArrUtil11.setIv_2nd(id_shortshort);
+        vibArrUtil11.setIv_3rd(id_shortlong);
+        vibArrUtil11.setIv_4th(id_longlong);
+        list.add(vibArrUtil11);
+
+        VibArrUtil vibArrUtil12 = new VibArrUtil();
+        vibArrUtil12.setIv_1st(id_shortlong);
+        vibArrUtil12.setIv_2nd(id_longlong);
+        vibArrUtil12.setIv_3rd(id_longshort);
+        vibArrUtil12.setIv_4th(id_shortshort);
+        list.add(vibArrUtil12);
+
+        VibArrUtil vibArrUtil13 = new VibArrUtil();
+        vibArrUtil13.setIv_1st(id_shortlong);
+        vibArrUtil13.setIv_2nd(id_longlong);
+        vibArrUtil13.setIv_3rd(id_shortshort);
+        vibArrUtil13.setIv_4th(id_longshort);
+        list.add(vibArrUtil13);
+
+        VibArrUtil vibArrUtil14 = new VibArrUtil();
+        vibArrUtil14.setIv_1st(id_shortlong);
+        vibArrUtil14.setIv_2nd(id_longshort);
+        vibArrUtil14.setIv_3rd(id_longlong);
+        vibArrUtil14.setIv_4th(id_shortshort);
+        list.add(vibArrUtil14);
+
+        VibArrUtil vibArrUtil15 = new VibArrUtil();
+        vibArrUtil15.setIv_1st(id_shortlong);
+        vibArrUtil15.setIv_2nd(id_longshort);
+        vibArrUtil15.setIv_3rd(id_shortshort);
+        vibArrUtil15.setIv_4th(id_longlong);
+        list.add(vibArrUtil15);
+
+        VibArrUtil vibArrUtil16 = new VibArrUtil();
+        vibArrUtil16.setIv_1st(id_shortlong);
+        vibArrUtil16.setIv_2nd(id_shortshort);
+        vibArrUtil16.setIv_3rd(id_longlong);
+        vibArrUtil16.setIv_4th(id_longshort);
+        list.add(vibArrUtil16);
+
+        VibArrUtil vibArrUtil17 = new VibArrUtil();
+        vibArrUtil17.setIv_1st(id_shortlong);
+        vibArrUtil17.setIv_2nd(id_shortshort);
+        vibArrUtil17.setIv_3rd(id_longshort);
+        vibArrUtil17.setIv_4th(id_longlong);
+        list.add(vibArrUtil17);
+
+        VibArrUtil vibArrUtil18 = new VibArrUtil();
+        vibArrUtil18.setIv_1st(id_shortshort);
+        vibArrUtil18.setIv_2nd(id_longlong);
+        vibArrUtil18.setIv_3rd(id_longshort);
+        vibArrUtil18.setIv_4th(id_shortlong);
+        list.add(vibArrUtil18);
+
+        VibArrUtil vibArrUtil19 = new VibArrUtil();
+        vibArrUtil19.setIv_1st(id_shortshort);
+        vibArrUtil19.setIv_2nd(id_longlong);
+        vibArrUtil19.setIv_3rd(id_shortlong);
+        vibArrUtil19.setIv_4th(id_longshort);
+        list.add(vibArrUtil19);
+
+        VibArrUtil vibArrUtil20 = new VibArrUtil();
+        vibArrUtil20.setIv_1st(id_shortshort);
+        vibArrUtil20.setIv_2nd(id_longshort);
+        vibArrUtil20.setIv_3rd(id_longlong);
+        vibArrUtil20.setIv_4th(id_shortlong);
+        list.add(vibArrUtil20);
+
+        VibArrUtil vibArrUtil21 = new VibArrUtil();
+        vibArrUtil21.setIv_1st(id_shortshort);
+        vibArrUtil21.setIv_2nd(id_longshort);
+        vibArrUtil21.setIv_3rd(id_shortlong);
+        vibArrUtil21.setIv_4th(id_longlong);
+        list.add(vibArrUtil21);
+
+        VibArrUtil vibArrUtil22 = new VibArrUtil();
+        vibArrUtil22.setIv_1st(id_shortshort);
+        vibArrUtil22.setIv_2nd(id_shortlong);
+        vibArrUtil22.setIv_3rd(id_longlong);
+        vibArrUtil22.setIv_4th(id_longshort);
+        list.add(vibArrUtil22);
+
+        VibArrUtil vibArrUtil23 = new VibArrUtil();
+        vibArrUtil23.setIv_1st(id_shortshort);
+        vibArrUtil23.setIv_2nd(id_shortlong);
+        vibArrUtil23.setIv_3rd(id_longshort);
+        vibArrUtil23.setIv_4th(id_longlong);
+        list.add(vibArrUtil23);
+
+
 
         vibArrAdapter.notifyDataSetChanged();
     }
